b81c75c6feea83acd2c7fa81318db5d13ca9b3fe
Maatyss <mvancaemelbecke@gmail.com>
2023-11-15 04:20:42 +0100

    clean index

--- src/index.js
+++ src/index.js
@@ -10,17 +10,4 @@ client.on('ready', (c) => {
   client.user.setActivity(`Self-Coding`)
 })
 
-// client.on('interactionCreate', async (interaction) => {
-//
-//   if (interaction.commandName === 'ping') {
-//     await interaction.deferReply({ephemeral: true})
-//     await interaction.editReply(`Pong ! Client: ${(await interaction.fetchReply()).createdTimestamp-interaction.createdTimestamp}ms | Websocket ${client.ws.ping}`)
-//   }
-//
-//   if (interaction.isButton()) {
-//     await handleRole(interaction)
-//   }
-// })
-
-
 client.login(process.env.TOKEN)
